3471a1291cb2bd35febc83d685bdb6188a1054dc
Zireael07 <zi@mail.com>
2018-08-13 16:07:23 +0200

    space rocks part 3, something to shoot

---
+++ enemy_ship.gd
@@ -0,0 +1,73 @@
+extends Area2D
+
+# class member variables go here, for example:
+export var rot_speed = 2.6 #radians
+export var thrust = 500
+export var max_vel = 400
+export var friction = 0.65
+export var max_speed = 100
+
+# motion
+var rot = 0
+var pos = Vector2()
+var vel = Vector2()
+var acc = Vector2()
+
+# bullets
+export(PackedScene) var bullet
+onready var bullet_container = $"bullet_container"
+#onready var bullet = preload("res://bullet.tscn")
+onready var gun_timer = $"gun_timer"
+
+var target = Vector2()
+var rel_pos = Vector2()
+
+func _ready():
+	add_to_group("enemy")
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+# using this because we don't need physics
+func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+	
+	#target
+	target = Vector2(-50,700)
+	
+
+	rel_pos = get_global_transform().xform_inv(target)
+	#print("Rel pos: " + str(rel_pos) + " abs y: " + str(abs(rel_pos.y)))
+	
+	var a = fix_atan(vel.x,vel.y)
+	
+	# movement happens!
+	#acc += vel * -friction
+	#vel += acc *delta
+	pos += vel * delta
+	set_position(pos)
+	
+	# rotation
+	#set_rotation(-a)
+
+func shoot():
+	gun_timer.start()
+	var b = bullet.instance()
+	bullet_container.add_child(b)
+	b.start_at(get_rotation(), $"muzzle".get_global_position())
+	
+	
+# AI
+# atan2(0,-1) returns 180 degrees in 3.0, we want 0
+# this counts in radians
+func fix_atan(x,y):
+	var ret = 0
+	var at = atan2(x,y)
+
+	if at > 0:
+		ret = at - PI
+	else:
+		ret= at + PI
+	
+	return ret
