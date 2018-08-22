6c6efb0ad4308dc40a3025603c93f174f4116a0d
Zireael07 <zi@mail.com>
2018-08-22 18:59:30 +0200

    enemy starbase will now shoot at the player

--- enemy_base.gd
+++ enemy_base.gd
@@ -7,10 +7,13 @@ export(PackedScene) var bullet
 onready var bullet_container = $"bullet_container"
 #onready var bullet = preload("res://bullet.tscn")
 onready var gun_timer = $"gun_timer"
+onready var explosion = preload("res://explosion.tscn")
 
 var targetted = false
 signal AI_targeted
 
+var shoot_target = null
+var shoot_rel_pos = Vector2()
 
 func _ready():
 	add_to_group("enemy")
@@ -18,7 +21,7 @@ func _ready():
 	#target
 	# it's static so we don't need to do it in process()
 	target = get_parent().get_position() + Vector2(200, -200)
-	
+	shoot_target = get_tree().get_nodes_in_group("player")[0].get_child(0)
 	
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -31,6 +34,16 @@ func _process(delta):
 
 	
 	#print("Target: " + str(target))
+	if shoot_target != null:
+		shoot_rel_pos = get_global_transform().xform_inv(shoot_target.get_global_position())
+	
+		if shoot_rel_pos.length() < 500:
+			if gun_timer.get_time_left() == 0:
+				shoot()
+	
+	
+	
+	#print(shoot_rel_pos)
 
 	rel_pos = get_global_transform().xform_inv(target)
 	#print("Rel pos: " + str(rel_pos) + " abs y: " + str(abs(rel_pos.y)))
@@ -59,12 +72,16 @@ func _process(delta):
 	# starbases are round so don't rotate...
 	# rotation
 	#set_rotation(-a)
+	
+	# redraw
+	update()
 
 func shoot():
 	gun_timer.start()
 	var b = bullet.instance()
 	bullet_container.add_child(b)
-	b.start_at(get_rotation(), $"muzzle".get_global_position())
+	var heading = fix_atan(shoot_rel_pos.x, shoot_rel_pos.y)
+	b.start_at(get_rotation() - heading, $"muzzle".get_global_position())
 
 # draw a red rectangle around the target
 func _draw():
@@ -74,6 +91,8 @@ func _draw():
 		draw_rect(rect, Color(1,0,0), false)
 	else:
 		pass
+		
+	draw_line(Vector2(0,0), shoot_rel_pos, Color(1,0,0))
 
 # click to target functionality
 func _on_Area2D_input_event(viewport, event, shape_idx):
