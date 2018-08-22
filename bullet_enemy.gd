353f5075a3e5612c3319849073c61bbb0b5cdd39
Zireael07 <zi@mail.com>
2018-08-22 18:59:10 +0200

    enemy bullets

---
+++ bullet_enemy.gd
@@ -0,0 +1,43 @@
+extends Area2D
+
+# class member variables go here, for example:
+var vel = Vector2()
+export var speed = 1000
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _physics_process(delta):
+	set_position(get_position() + vel * delta)
+
+func start_at(dir, pos):
+	# bullet's pointing to the side by default while the ship's pointing up
+	set_rotation(dir-PI/2)
+	set_position(pos)
+	# pointing up by default
+	vel = Vector2(0,-speed).rotated(dir)
+
+func _on_lifetime_timeout():
+	queue_free()
+
+func _on_bullet_area_entered( area ):
+	if area.get_parent().get_groups().has("player"):
+		queue_free()
+		print(area.get_parent().get_name())
+
+		var pos = area.get_global_position()
+
+		# kill the player
+		# reenable when it doesn't destroy the game
+		#area.get_parent().queue_free()
+
+		# explosion
+		var expl = get_parent().get_parent().explosion.instance()
+		get_parent().get_parent().add_child(expl)
+		expl.set_global_position(pos)
+		expl.play()
+		
+		# bugfix
+		#get_parent().get_parent().shoot_target = null
