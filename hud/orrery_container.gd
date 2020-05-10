5a1d8b769f94d6778db98e7246a6d1eaece66ca8
Zireael07 <zi@mail.com>
2020-05-10 15:16:22 +0200

    fix: rescale system map for Saturn to fit

--- hud/orrery_container.gd
+++ hud/orrery_container.gd
@@ -93,7 +93,10 @@ func _ready():
 	set_clip_contents(true)
 	
 	# set cntr
-	get_child(0).set_cntr(Vector2(80,80))
+	if self.get_name() == "map view":
+		get_child(0).set_cntr(Vector2(80,80))
+		zoom_scale = stars[0].custom_orrery_scale*1.6
+		
 
 func _process(_delta):
 #	# Called every frame. Delta is time since last frame.
