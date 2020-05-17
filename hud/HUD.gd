7095d63244f3e8468d9dab1495561d28a7ca7381
Zireael07 <zi@mail.com>
2020-05-17 19:13:36 +0200

    new: friendly ship view tells you which planet the AI is orbiting

--- hud/HUD.gd
+++ hud/HUD.gd
@@ -549,6 +549,9 @@ func _on_ButtonShip_pressed():
 		if target.is_in_group("friendly"):
 			$"Control2/Panel_rightHUD/PanelInfo/ShipInfo/Task".show()
 			var task = "Task: " + str(target.brain.tasks[target.brain.curr_state])
+			if target.brain.curr_state == 2:
+				var p = target.brain.get_state_obj().planet_
+				task += " " + str(p.get_node("Label").get_text() )
 			if target.brain.curr_state == 5:
 				var id = target.brain.get_state_obj().planet_
 				var p = get_tree().get_nodes_in_group("planets")[id-1]
