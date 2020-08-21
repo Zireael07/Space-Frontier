ab9c361e8734c161222cd2a12a4344c8ce4513ce
Zireael07 <zi@mail.com>
2020-08-21 16:43:32 +0200

    new: implement attack my target order

--- hud/OrdersControl.gd
+++ hud/OrdersControl.gd
@@ -14,6 +14,10 @@ func _ready():
 	var size = Vector2(100,100)*get_node("TextureButton").rect_scale
 	#print("Size: " + str(size))
 	_set_size(size)
+	if game.player.HUD.target != null:
+		get_node("VBoxContainer/Button2").set_disabled(false)
+	else:
+		get_node("VBoxContainer/Button2").set_disabled(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -36,4 +40,14 @@ func _on_Button_pressed():
 		brain.target = game.player.get_child(0).get_global_position()
 		brain.set_state(brain.STATE_IDLE)
 		
+	#pass # Replace with function body.
+
+
+func _on_Button2_pressed():
+	if get_node("VBoxContainer/Button").visible:
+		var area = game.player.HUD.ship_to_control[self]
+		print("Issuing order 2 to ship " + area.get_parent().get_name())
+		# actually issue order
+		var brain = area.get_node("brain")
+		brain.set_state(brain.STATE_ATTACK, game.player.HUD.target)
 	#pass # Replace with function body.
