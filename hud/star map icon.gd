ad00cff2c2e512b4f99003c15b58ec799f3ad7ce
Zireael07 <zi@mail.com>
2021-03-07 15:08:15 +0100

    new: starmap icon for Luyten 726-8

--- hud/star map icon.gd
+++ hud/star map icon.gd
@@ -16,10 +16,23 @@ func _ready():
 	set_position(Vector2(x*50, y*50))
 	# positive depth (above the plane) is negative y
 	get_node("Line2D").points[0] = Vector2(18, -depth*50)
+	
 	var depth_s = sign(-depth)
 	# 18 is the height of the star icon
 	get_node("TextureRect3").rect_position = Vector2(0, -depth*50+18*depth_s) 
-	get_node("Label").rect_position = Vector2(-6.5, -depth*50+18)
+	# name label
+	# above the plane (place next to star icon)
+	if depth_s < 0:
+		get_node("Label").rect_position = Vector2(-6.5, -depth*50+18)
+	# below the plane (place next to "shadow" icon)
+	else:
+		get_node("Label").rect_position = Vector2(0, 0)
+	
+	# Z axis label if needed
+	if depth < -8: 
+		get_node("Label2").rect_position = Vector2(0, 25)
+		get_node("Label2").set_text("Z: " + str(depth) + " ly")
+		get_node("Label2").show()
 	
 	set_name(named)
 	get_node("Label").set_text(named)
