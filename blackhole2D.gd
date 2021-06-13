f528271e020e9fca8d48f95e053e03a948b12cb8
Zireael07 <zi@mail.com>
2021-06-13 14:02:54 +0200

    fix: wormholes should only be triggered by player

--- blackhole2D.gd
+++ blackhole2D.gd
@@ -49,13 +49,14 @@ func jump():
 
 
 func _on_Area2D_area_entered(_area):
-	if not entered and active:
-		var system = get_tree().get_nodes_in_group("main")[0].curr_system
-		print("Wormhole entered in system: ", system)
-		entered = true
-		
-		game.player.w_hole = self
-		game.player.HUD.show_starmap()
+	if _area.get_parent().get_groups().has("player"):
+		if not entered and active:
+			var system = get_tree().get_nodes_in_group("main")[0].curr_system
+			print("Wormhole entered in system: ", system)
+			entered = true
+			
+			game.player.w_hole = self
+			game.player.HUD.show_starmap()
 		
 
 
