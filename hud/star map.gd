58437ef9b7924fbc427e7a2c45e0b3641176f020
Zireael07 <zi@mail.com>
2022-07-04 14:27:26 +0200

    new: test function that converts from right angle and declension to Winchell Chung's galactic xyz

--- hud/star map.gd
+++ hud/star map.gd
@@ -25,6 +25,17 @@ func _ready():
 			ic.y = strip_units(str(line[2]))
 			ic.depth = strip_units(str(line[3]))
 			
+			# test ra-dec conversion
+			if line[5] != "" and line[6] != "" and line[7] != "":
+				if "h" in line[5] and not "m" in line[5]:
+					# if no minutes specified, we assume decimal hours
+					# 15 degrees in an hour (360/24) 
+					var ra_deg = float(15*float(line[5].rstrip("h")))
+					# for now, assume degrees are given in decimal degrees
+					var dec = float(line[6])
+					galactic_from_ra_dec(ra_deg, dec, float(line[7]))
+					# we need to minus the y coordinate for our uses for some reason?
+			
 			get_node("Control").add_child(ic)
 	
 	
@@ -35,6 +46,29 @@ func strip_units(entry):
 	elif "pc" in entry:
 		num = float(entry.rstrip("pc"))
 	return num
+
+# based on Winchell Chung's Star3D spreadsheet and 
+# http://starmap.whitten.org/files/src/gal_pl.txt
+# input in degrees by default!!!
+func galactic_from_ra_dec(ra, dec, dist):
+	# Find Equatorial cartesian coordinates
+	ra = deg2rad(ra)
+	dec = deg2rad(dec)
+	# dec and ra in radians from here on
+	var rvect = dist * cos(dec);
+
+	var equat_x = rvect * cos(ra);
+	var equat_y = rvect * sin(ra);
+	var equat_z = dist * sin(dec);
+	
+	# Find Galactic cartesian coordinates
+	var xg = -(.055 * equat_x) - (.8734 * equat_y) - (.4839 * equat_z);
+	var yg =  (.494 * equat_x) - (.4449 * equat_y) + (.747 * equat_z);
+	var zg = -(.8677 * equat_x) - (.1979 * equat_y) + (.4560 * equat_z);
+	
+	var gal = Vector3(xg, yg, zg)
+	print("Galactic coords: ", gal)
+	return gal
 
 func load_data():
 	var file = File.new()
