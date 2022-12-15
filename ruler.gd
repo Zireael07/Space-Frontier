e40b6fb85500cb6d416eea3c3afa622ea1e40e8a
Zireael07 <zi@mail.com>
2022-12-15 14:31:08 +0100

    new: implement a small ruler tool

---
+++ ruler.gd
@@ -0,0 +1,28 @@
+tool
+extends Control
+
+
+# Declare member variables here. Examples:
+export var pts = []
+var text = ""
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var mid = pts[0]+(pts[1]-pts[0])/2
+	
+	$Label.set_position(mid)
+	
+	if text == "":
+		var dist = pts[1].distance_to(pts[0])
+		$Label.set_text("%2.f" % dist)
+	else:
+		$Label.set_text(text)
+	$Line2D.points = pts
+	
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
