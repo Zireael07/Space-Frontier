7304d6149132ab2396018795725466640c6bd521
Zireael07 <zi@mail.com>
2023-01-17 17:24:36 +0100

    new: commit a testing scene

---
+++ test_label.gd
@@ -0,0 +1,22 @@
+tool
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var siz = get_node("TextureRect").get_size()
+	get_node("TextureRect").set_position(-(siz/2))
+	get_node("TextureRect").set_pivot_offset(siz/2)
+	get_node("Label").set_position(siz/2-Vector2(20,7))
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
